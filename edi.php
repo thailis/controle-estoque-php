5fdf018e4c5028339b1a2fb7d6395a9d261e5073
Thailis <thailis.r@gmail.com>
2026-09-18 12:26:30 +0000

    edi

--- edi.php
+++ edi.php
@@ -754,7 +754,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     </style>
 </head>
 <body>
-    <div class="container" style="max-width: 1180px;">
+    <div class="container" style="max-width: 1700px; min-width: 1700px;">
         <nav class="d-flex flex-wrap gap-2 mb-3" aria-label="Navegação do sistema">
                 <a class="btn btn-outline-secondary btn-sm" href="index.php">🏠 Dashboard</a>
             <a class="btn btn-outline-secondary btn-sm" href="estoque.php">Estoque</a>
