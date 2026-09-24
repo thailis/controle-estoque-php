52b3adb3ac85c15b653cbaed52ecfb3b3490f3ff
Thailis <thailis.r@gmail.com>
2026-09-24 13:46:09 +0000

    BOM: ajustar largura do quadro para caber as novas colunas

--- bomnova.php
+++ bomnova.php
@@ -606,7 +606,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     </style>
 </head>
 <body>
-    <div class="container-fluid" style="max-width: 1600px;">
+    <div class="container-fluid" style="max-width: 98vw;">
         <nav class="d-flex flex-wrap gap-2 mb-3" aria-label="Navegação do sistema">
                 <a class="btn btn-outline-secondary btn-sm" href="index.php">🏠 Dashboard</a>
             <a class="btn btn-outline-secondary btn-sm" href="estoque.php">Estoque</a>
